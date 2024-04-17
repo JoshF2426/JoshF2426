9a6b546d9bfb9417151ad18b4b68869c5514b983
Joshua Flores <joshjf616@gmail.com>
2024-04-17 19:37:29 -0400

    hi

---
+++ form.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 5500;
+
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'index.html'); // send HTML file on GET request
+});
+
+app.post('/submit-form', (req, res) => {
+    const username = req.body.username; // access form data
+    // Add validation logic here
+    res.send(`Success!`);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
