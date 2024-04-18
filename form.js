09ac3bebad3f8123ae4d703e7ab32ab15e896b9d
Joshua Flores <joshjf616@gmail.com>
2024-04-17 20:11:49 -0400

    test working

--- form.js
+++ form.js
@@ -5,13 +5,13 @@ const port = 5500;
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index.html'); // send HTML file on GET request
-});
+    res.sendFile(__dirname + '/contact.html'); // send HTML file on GET request
+}); 
 
 app.post('/submit-form', (req, res) => {
     const username = req.body.username; // access form data
     // Add validation logic here
-    res.send(`Success!`);
+    res.send(`Success`);
 });
 
 app.listen(port, () => {
